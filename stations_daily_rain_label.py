16209ad5a86e2ea720502c1bbe295be1ece668da
om990301 <m10815102@gapps.ntust.edu.tw>
2021-11-16 13:04:01 +0800

    Update stations_daily_rain_label.py

--- stations_daily_rain_label.py
+++ stations_daily_rain_label.py
@@ -11,7 +11,13 @@ from openpyxl import load_workbook
 
 #########################################
 
-station_path = '/media/ubuntu/My Passport/NCDR/ncdr_rain_predict/data/station_data'
+station_path = '/media/ubuntu/My Passport/NCDR/Data/station_data'
+
+Result = 'Result/daily_rain.xlxs'
+
+min_rain = 0
+
+thresh_hold = 50
 
 
 special_station_input_id = ['C0V250','C0R140']
@@ -26,6 +32,8 @@ print("[INFO] loading special station")
 tmp_special_stations = []
 data_special_stations = []
 
+date_dic = {}
+
 for year in os.listdir(station_path):
     #print(file)
     year_dir = station_path + "/" + year
@@ -33,25 +41,24 @@ for year in os.listdir(station_path):
         month_dir = year_dir + "/" + month
         for date in os.listdir(month_dir):
             date_dir = month_dir + "/" + date
+            afternoon_rainfall = 0
             for date_file in os.listdir(date_dir):
                 if not date_file.endswith(".txt"):
                     #break
                     continue
                 time = int(date_file[-6:-4])
-                if time >11:
+                if time <11:
                     continue
                 file_name = date_file
                 date_txt = date_dir + "/" + date_file
-                #print(date_txt)
+                print(date_txt)
                 f = open(date_txt)
                 
-                stations = []
-                lons = []
-                lats = []
-                elevs = []
-                temps = []
-                huminitys = []
-                wind_directions = []
+                station = -1
+                
+                tmp_water = -1
+                
+                stations_daily_rain = {}
                 
                 for line in f.readlines():
                     line = line.replace("-", " -")
@@ -60,21 +67,27 @@ for year in os.listdir(station_path):
                         continue
                     else:
                         # parsing append list
-                        stations.append(parsing[0])
-                        lons.append(float("{:.2f}".format(float(parsing[1]))))
-                        lats.append(float("{:.2f}".format(float(parsing[2]))))
-                        #elevs.append(float("{:.4f}".format(float(parsing[3]))))
-                        elevs.append(float(parsing[3]))
-                
-                        temps.append(float(parsing[5]))
-                
-                        huminitys.append((float(parsing[6])))
-                        wind_directions.append((float(parsing[7])))
+                        station = parsing[0]
+                        tmp_water= float(parsing[9])
+                        if tmp_water<min_rain:
+                            tmp_water = min_rain
                         
-                for num in range(len(stations)):
-                    tmp_special_stations.append([huminitys[0],temps[0],wind_directions[0]])
-                data_special_stations.append(tmp_special_stations)
-                tmp_special_stations = []
-data_special_stations = np.array(data_special_stations)
-data_special_stations = np.reshape(data_special_stations,(-1,station_time,len(special_station_input_id)*3))
-print("number of videos: ", data_special_stations.shape)
+                        if station in special_station_input_id:
+                            if stations_daily_rain.get(station) == "None":
+                                stations_daily_rain[station] = tmp_water
+                            else:
+                                stations_daily_rain[station] += stations_daily_rain[station]
+                
+                all_larger = 1
+                
+                for rain in stations_daily_rain:
+                    if rain < thresh_hold:
+                        all_larger = 0
+                        break
+                
+                if all_larger:
+                    print("1")
+                else:
+                    print("0")
+                
+                
