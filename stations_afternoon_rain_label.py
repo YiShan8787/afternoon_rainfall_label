49f5152e47b9e46e1fe3b905a871ae3bbb8377c5
om990301 <m10815102@gapps.ntust.edu.tw>
2021-11-17 13:28:13 +0800

    Update stations_afternoon_rain_label.py

--- stations_afternoon_rain_label.py
+++ stations_afternoon_rain_label.py
@@ -98,6 +98,9 @@ for year in os.listdir(station_path):
             all_larger = 1
             
             for station_id in special_station_input_id:
+                print(stations_morning_rain[station_id])
+                print(stations_morning_rain[station_id])
+                print(stations_night_rain[station_id])
                 if stations_afternoon_rain[station_id] < stations_morning_rain[station_id] + stations_night_rain[station_id]:
                     
                     all_larger = 0
