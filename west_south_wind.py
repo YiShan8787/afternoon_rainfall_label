f60a67ccd2f332dc8dfbbb128d746bc6590b24e4
om990301 <m10815102@gapps.ntust.edu.tw>
2021-12-12 16:53:23 +0800

    Update west_south_wind.py

--- west_south_wind.py
+++ west_south_wind.py
@@ -82,11 +82,12 @@ for date in os.listdir(U_path):
                 else:
                     data_U.append(float(parsing[0]))
                     
-            data_U = np.array(data_U)
-            
+                    
             if not data_U:
                 data_U = np.zeros((41,71))
-            data_U = np.reshape(data_U,(41,71))
+            else:
+                data_U = np.array(data_U)
+                data_U = np.reshape(data_U,(41,71))
             
             #get the value in the selected wind field
             for i in width_list:
@@ -122,10 +123,11 @@ for date in os.listdir(V_path):
                 else:
                     data_V.append(float(parsing[0]))
                     
-            data_V = np.array(data_V)
             if not data_V:
                 data_V = np.zeros((41,71))
-            data_V = np.reshape(data_V,(41,71))
+            else:
+                data_V = np.array(data_V)
+                data_V = np.reshape(data_V,(41,71))
             
             #get the value in the selected wind field
             for i in width_list:
